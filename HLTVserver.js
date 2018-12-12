911ba9e42f2b32a5d64122bb00395c6536b3d5d7
tchen <tchen@paragontesting.ca>
2018-12-12 10:48:22 -0800

    Removed old weather app, refactored HLTVMain into more reasonable sub-views; refactored HLTVserver.js

--- HLTVserver.js
+++ HLTVserver.js
@@ -13,27 +13,27 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-    res.render('HLTVMain', {matches: null, error: null});
+    res.render('landing');
 })
 
 app.get('/matches/scorebot', function (req, res) {
-    res.render('scorebot', {id: req.id, error: null});
+    res.render('scorebot', {id: req.query.id, error: null});
 })
 
 app.get('/matches/matchanalysis', function (req, res) {
-    res.render('matchanalysis', {id: req.id, error: null});
+    res.render('matchanalysis', {id: req.query.id, error: null});
 })
 
 app.get('/matches', function (req, res) {
     HLTV.getMatches().then((answer) => {
         let matches = answer
         if(matches == undefined){
-            res.render('HLTVMain', {matches: null, error: 'Error, please try again'});
+            res.render('matches', {matches: null, error: 'Error, please try again'});
         } else {
-            res.render('HLTVMain', {matches: matches, error: null});
+            res.render('matches', {matches: matches, error: null});
         }
     }).catch(err => {
-        res.render('HLTVMain', {matches: null, error: 'Error, please try again'});
+        res.render('matches', {matches: null, error: 'Error, please try again'});
         console.log(err)
     });
     console.log("requested matches"); 
@@ -43,17 +43,17 @@ app.get('/results', function (req, res) {
     HLTV.getMatches().then((answer) => {
         let matches = answer
         if(matches == undefined){
-            res.render('HLTVMain', {matches: null, error: 'Error, please try again'});
+            res.render('results', {matches: null, error: 'Error, please try again'});
         } else {
-            res.render('HLTVMain', {matches: matches, error: null});
+            res.render('results', {matches: matches, error: null});
         }
     }).catch(err => {
-        res.render('HLTVMain', {matches: null, error: 'Error, please try again'});
+        res.render('results', {matches: null, error: 'Error, please try again'});
         console.log(err)
     });
     console.log("requested matches"); 
 })
 
-app.listen(3001, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
