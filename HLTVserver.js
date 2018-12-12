fb8de999398f99f1c2da3989d36c74052efcdbda
tchen <tchen@paragontesting.ca>
2018-12-11 17:13:17 -0800

    Matches page done, stubs created for analysis and scorebot

--- HLTVserver.js
+++ HLTVserver.js
@@ -11,13 +11,35 @@ HLTV.createInstance({hltvUrl: 'localhost', loadPage: https.get})
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-    
+
 app.get('/', function (req, res) {
     res.render('HLTVMain', {matches: null, error: null});
 })
 
-app.post('/', function (req, res) {
+app.get('/matches/scorebot', function (req, res) {
+    res.render('scorebot', {id: req.id, error: null});
+})
 
+app.get('/matches/matchanalysis', function (req, res) {
+    res.render('matchanalysis', {id: req.id, error: null});
+})
+
+app.get('/matches', function (req, res) {
+    HLTV.getMatches().then((answer) => {
+        let matches = answer
+        if(matches == undefined){
+            res.render('HLTVMain', {matches: null, error: 'Error, please try again'});
+        } else {
+            res.render('HLTVMain', {matches: matches, error: null});
+        }
+    }).catch(err => {
+        res.render('HLTVMain', {matches: null, error: 'Error, please try again'});
+        console.log(err)
+    });
+    console.log("requested matches"); 
+})
+
+app.get('/results', function (req, res) {
     HLTV.getMatches().then((answer) => {
         let matches = answer
         if(matches == undefined){
